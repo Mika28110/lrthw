afc67a65ca3c0d29628fa8501b555357630ebaa3
michael.godinhodacosta@hotmail.fr <michael.godinhodacosta@hotmail.fr>
2015-05-26 21:50:13 +0200

    "Suite"

---
+++ ex6.rb
@@ -0,0 +1,29 @@
+types_of_people = 10 #number of people
+x = "There are #{types_of_people} types of people." #variable containing a text
+binary = "binary" #variable containing a string
+do_not = "don't" #variable containing a string
+y = "Those who know #{binary} and those who #{do_not}." #variable containing a string
+
+puts x #shows the value of the x variable
+puts y #shows the value of the y variable
+
+puts "I said: #{x}." #shows a text with the x variable
+puts "I also said: '#{y}'." #shows a text with the y variable
+
+hilarious = false #variable hilarious with 'false' bolean value
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}" #variable containing a text
+
+puts joke_evaluation #shows the value of the joke_evaluation variable
+
+w = "This is the left side of..." #variable with a string
+e = "a string with a right side." #variable with a string
+
+puts w + e #shows the association of the w and the e variables
+
+#texts put inside a string on lines 2, 5, 10, 11, 14
+
+#you were lying the are more than 4
+
+#the sign "+" associate two entities
+
+#it doesn't work with "'" because there is single-quote in the word "Isn't" so the program thinks that the text ends after the "n" of "isn't".
---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb." #shows a text
+puts "Its fleece was white as #{'snow'}." #shows a text
+puts "And everywhere that Mary went." #shows a text
+puts "." * 10  # what'd that do? #mutiple a point by 10
+
+end1 = "C" #string in a variable
+end2 = "h" #string in a variable
+end3 = "e" #string in a variable
+end4 = "e" #string in a variable
+end5 = "s" #string in a variable
+end6 = "e" #string in a variable
+end7 = "B" #string in a variable
+end8 = "u" #string in a variable
+end9 = "r" #string in a variable
+end10 = "g" #string in a variable
+end11 = "e" #string in a variable
+end12 = "r" #string in a variable
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6 #shows the variables from end1 to end6
+puts end7 + end8 + end9 + end10 + end11 + end12 #shows the variables from end7 to end12
